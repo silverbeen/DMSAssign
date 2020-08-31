c5f254f2e11be5992b0d470988d4f30a28b6198d
강은빈 <kub0803@naver.com>
2020-08-31 22:40:35 +0900

    todolist

--- 20.08.07/list.js
+++ 20.08.07/list.js
@@ -1,19 +1,20 @@
-const todo={
-    inputWrap: document.getElementById("intputWrap"),
+const todo = {
+    todoInput: document.getElementById("text"),
     add: document.getElementById("add"),
     list: document.getElementById("list"),
-};
+  };
 
-function delateMe(e){
-    e.currentTaget.parentNode.remove();
-};
+  function deleteMe(e) {
+    e.currentTarget.parentNode.remove();
+  };
 
-todo.add.addEventListener("chick," () => {
-    const todoTemplate =` <li class="itme">
+  todo.add.addEventListener("click", () => {
+    const todoTemplate = `<li class="itme">
     <img class="itmeclose" src="./close.png" onclick="delereMe(event);" alt="delate" title="delate">
     <input class="imtecheckbox" type="checkbox">
-    <span class="itmeText">${todo.todoInput.value}</span>
+    <span class="itmeText">${todo.text.vl}</span>
+    
 </li>`;
 
     todo.list.insertAdjacentHTML("beforeend", todoTemplate);
-});
+  });
